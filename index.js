a1b5bc48b6f0780bda1f77f644041560324c149c
TakterNetiwut <netiwut.ka.58@ubu.ac.th>
2018-12-10 20:54:59 +0700

    no message

--- index.js
+++ index.js
@@ -10,35 +10,6 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.post('/webhooks', (req, res) => {
-    //let reply_token = req.body.events[0].replyToken
-    //let msg = req.body.events[0].message.text
-    //reply(reply_token, msg)
-    res.sendStatus(200)
-})
+app.get('/webhooks', (req, res) => res.sendStatus(200))
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-function reply(reply_token) {
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {h3MOG4XttJfbMVHWMLjavfqiiNxix9LqFhujfrCMI7Fp2IaFJp9ONJYOyM6y4g+nY6hpjvk0UhwS/AgmPfenIeSsXQWVm1o3pm9KdrlDhI46+sSjRXOwzhTgIVbiMrbsLJ97VtvoBoMAui+B5T2q+AdB04t89/1O/w1cDnyilFU=}'
-    }
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
-        }]
-    })
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
-}
