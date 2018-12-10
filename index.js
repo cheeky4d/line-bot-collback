fba8b8af95c9a449c4a8df709e27700117f0bbca
TakterNetiwut <netiwut.ka.58@ubu.ac.th>
2018-12-10 21:44:20 +0700

    no message

--- index.js
+++ index.js
@@ -5,36 +5,49 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-    .use(bodyParser.urlencoded({ extended: false }))
-    .use(bodyParser.json())
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .post('/', (req, res) => {
-      let reply_token = req.body.events[0].replyToken
-      let msg = req.body.events[0].message.text
-      reply(reply_token, msg)
-      res.sendStatus(200)
-    })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(bodyParser.json())
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .post('/', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
+    res.sendStatus(200)
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-  function reply(reply_token, msg) {
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {4EsKNf/CN1c5fUaTuyINPM7Q7nizwxZRGceXgLhZyYR4DV/03j8VqnmPiyEjfrdWY6hpjvk0UhwS/AgmPfenIeSsXQWVm1o3pm9KdrlDhI5YeZdWX/mOkpD2XFzdG+TG4EEoay0VcPbtlW8WPFBS1QdB04t89/1O/w1cDnyilFU=}'
-    }
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: msg
-        }]
-    })
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
+function reply(reply_token, msg) {
+  let headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer {4EsKNf/CN1c5fUaTuyINPM7Q7nizwxZRGceXgLhZyYR4DV/03j8VqnmPiyEjfrdWY6hpjvk0UhwS/AgmPfenIeSsXQWVm1o3pm9KdrlDhI5YeZdWX/mOkpD2XFzdG+TG4EEoay0VcPbtlW8WPFBS1QdB04t89/1O/w1cDnyilFU=}'
+  }
+  let body = JSON.stringify({
+    replyToken: reply_token,
+    messages: [{
+      type: 'text',
+      text: messages(msg)
+    }]
+  })
+  request.post({
+    url: 'https://api.line.me/v2/bot/message/reply',
+    headers: headers,
+    body: body
+  }, (err, res, body) => {
+    console.log('status = ' + res.statusCode);
+  });
+}
+
+function messages(msg) {
+  switch (msg) {
+    case "สวัสดี":
+      return "หวัดดีไอสัส"
+    case "ทำอะไรอยู่":
+      return "อ่านหนังสือ"
+    case "ชื่ออะไร":
+      return "ชื่อไรก็ได้โตแล้ว"
+    default:
+      return "พิมพ์ไรมาสัส กูโง่ พิมพ์มาใหม่"
+  }
 }
