d2e8c0ba7185ab3530f3cd9432111ca79df0470c
Netiwut <36693648+Netiwut@users.noreply.github.com>
2018-12-10 21:14:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-//const request = require('request')
+const request = require('request')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
