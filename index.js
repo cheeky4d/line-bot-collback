1c2aeed77dd1334b92bc604b61c9a260de1fbdad
TakterNetiwut <netiwut.ka.58@ubu.ac.th>
2018-12-10 21:04:29 +0700

    no message

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .post('/', (req, res) => {res.sendStatus(200)})
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json())
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .post('/', (req, res) => {
+
+      res.sendStatus(200)
+    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
