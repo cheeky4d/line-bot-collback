8a2a3d43bd729617079845012bfcae7ccf1ad343
Netiwut <36693648+Netiwut@users.noreply.github.com>
2018-12-10 21:18:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-    //.use(bodyParser.urlencoded({ extended: false }))
-    //.use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json())
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
