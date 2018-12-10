13e89ba72b15f340d6d220b08878975f7bdbabe5
TakterNetiwut <netiwut.ka.58@ubu.ac.th>
2018-12-10 21:01:18 +0700

    no message

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
 const path = require('path')
-
-const app = express()
 const PORT = process.env.PORT || 5000
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.post('/webhooks', (req, res) => res.sendStatus(200))
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .post('/', (req, res) => res.sendStatus(200))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
