750b7e00da698ab5b60c3c29b377317426c5ca58
TakterNetiwut <netiwut.ka.58@ubu.ac.th>
2018-12-10 20:57:39 +0700

    no message

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/webhooks', (req, res) => res.sendStatus(200))
+app.post('/webhooks', (req, res) => res.sendStatus(200))
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
