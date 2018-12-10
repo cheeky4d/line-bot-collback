95a93e98322853e00b91ae9baf0e0c53f9ac6f6a
TakterNetiwut <netiwut.ka.58@ubu.ac.th>
2018-12-10 20:18:57 +0700

    no message

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 app.post('/webhooks', (req, res) => res.sendStatus(200))
-app.get('/', (req, res) => res.end('<h1>Hello World</h1>');)
+app.get('/', (req, res) => res.end('<h1>Hello World</h1>'))
 
 app.listen(4000, () => console.log('Server started on port 4000'));
